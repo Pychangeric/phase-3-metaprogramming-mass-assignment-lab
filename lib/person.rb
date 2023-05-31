0fe77b5d2c61f709233aa0172995bd0be38bd3b1
pychangeric <pychangeric@gmail.com>
2023-05-31 08:04:13 +0300

    done

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,14 @@
 class Person
-  # your code here
+  bob_attributes = { name: "Bob", hair_color: "Brown" }
+
+  bob = Person.new(bob_attributes)
+  bob.name       # => "Bob"
+  bob.hair_color # => "Brown"
+  
+  susan_attributes = { name: "Susan", height: "5'11\"", eye_color: "Green" }
+  
+  susan = Person.new(susan_attributes)
+  susan.name      # => "Susan"
+  susan.height    # => "5'11""
+  susan.eye_color # => "Green"
 end
